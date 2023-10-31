2f98dd9cdc581a82d0a74b05fcb752e635298e27
Marcel <marcel.soto324@gmail.com>
2023-10-31 22:41:54 +0100

    last_commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,6 +11,12 @@ export class HeaderComponent {
   espanolURL: string = 'https://drive.google.com/file/d/1wFRo47p6Owsq8GmT_urYPv6XZJSr-UEn/view?usp=sharing';
   englishURL: string = 'https://drive.google.com/file/d/1GKtIWFt2K6sKTMMCWyxKOx9FjoUbzkWK/view?usp=sharing';
 
+
+  //-->> Función para cambiar el CV. Si redirectToCV cuyo valor por defecto es'Mi CV',
+  // toma el valor de español o inglés, usando windows.location.href que es un parametro usado
+  // para redirección, tomaría el valor de la url según sea el caso y nos llevaría al enlace correspondiente.
+  // para ello, definimos arriba las propiedades espanolURL y englishURL que contienen los enlaces.
+
   redirectToCV() {
     if (this.selectedCV === 'español') {
       window.location.href = this.espanolURL;
